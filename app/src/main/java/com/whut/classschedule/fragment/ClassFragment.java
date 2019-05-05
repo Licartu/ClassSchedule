d622e6361b229b79ec176b5fe5d5ebc1f28b0592
Licartu <794189661@qq.com>
2019-05-05 22:49:55 +0800

    第二次提交

---
+++ app/src/main/java/com/whut/classschedule/fragment/ClassFragment.java
@@ -0,0 +1,10 @@
+package com.whut.classschedule.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Lenovo on 2019/4/30.
+ */
+
+public class ClassFragment extends Fragment {
+}
